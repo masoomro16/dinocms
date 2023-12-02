26fe05a0d57332f7cd21ce8f1a5ab75b15185bd7
Maira Soomro <masoomro16@ymail.com>
2023-12-02 13:54:49 -0800

    Added salt

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'd9b0df66ff97a666027e665707b4e3e7'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'b79c84a0accf67788c73e1c1fdafba64'),
   },
